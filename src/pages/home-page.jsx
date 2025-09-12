3f2efae94307ed5d7583fa5397350724045ef5cb
Muhammad Umair <umairchughtai92@gmail.com>
2025-09-12 14:35:59 +0500

    set the button route in home page

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { FormBackground } from "../components/form-background";
+import { useNavigate } from "react-router-dom";
 
 export function HomePage() {
+  const navigate = useNavigate();
   return (
     <>
       <main className="relative h-[80vh] flex flex-col justify-center items-center gap-4 mt-14">
@@ -15,10 +17,8 @@ export function HomePage() {
           <div className="pt-4">
             <Button
               size="lg"
-              className="px-8 py-3 text-lg rounded-full"
-              onClick={() => {
-                window.location.href = "/playground";
-              }}
+              className="px-8 py-3 text-lg rounded-full pointer-coarse:"
+              onClick={() => navigate("/playground")}
             >
               Go To Playground
             </Button>
